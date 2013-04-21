31588eb40e2c3f5bca7e43360fe46f5294cd5df1
smc002 <sunmengchao@gmail.com>
2013-04-21 21:50:04 +0800

    testfile. bug fix

--- testomp.m
+++ testomp.m
@@ -1,10 +1,10 @@
-function [nonzeros res_forBit]=testomp;
+function testomp;
 !./ompbit
 load Res_forBit0.mat
-%load Res_forBit1.mat
-%load Res_forBit2.mat
-%load Res_forBit3.mat
+load Res_forBit1.mat
+load Res_forBit2.mat
+load Res_forBit3.mat
+res=res0+res1+res2+res3;
+load Res_forBit.mat
 %res_forBit = res_forBit0 + res_forBit1 + res_forBit2 + res_forBit3;
-res_forBit = res_forBit0;
-res_forBit = res_forBit' + res_forBit;
-nonzeros = nnz(res_forBit)
+nnz(res_forBit-res)
